7af41d4eb97cb596bc851763903ef3abd2d9b35b
zhongjianru <459251261@qq.com>
2020-04-01 23:10:39 +0800

    commit

--- words.py
+++ words.py
@@ -2,10 +2,38 @@ import jieba
 import pandas as pd
 
 
-def show_txt_words():
+# 1、通过键值对的形式存储词语及其出现的次数
+def show_txt_words_f1():
     txt = open("C:\\Users\\Zero\\Desktop\\tools\\Python数据科学手册_机器学习.md", "r", encoding='utf-8').read()
     words = jieba.lcut(txt)  # 使用精确模式对文本进行分词
-    # counts = {}  # 通过键值对的形式存储词语及其出现的次数
+    counts = {}
+
+    for word in words:
+        if len(word) == 1:  # 对分隔后的文本进行计数，单个词语不计算在内
+            continue
+        else:
+            counts[word] = counts.get(word, 0) + 1
+
+    print(counts)  # 键值对：{'数据': 55, '标签': 29, '可以': 24, '学习': 22, '模型': 22}
+
+    items = list(counts.items())  # 将键值对转换成列表
+    items.sort(key=lambda x: x[1], reverse=True)  # 根据词语出现的次数对列表进行倒序排列，list.sort()，x[0]=word,x[1]=count
+
+    print(items)  # 列表：[('数据', 55), ('标签', 29), ('可以', 24), ('学习', 22), ('模型', 22)]
+
+    # 输出前 20
+    for i in range(20):
+        word, count = items[i]  # 分别将 items[i].word 和 items[i].count 赋值给变量
+        print("{0:<5}{1:>5}".format(word, count))
+        # 数据 55
+        # 标签 29
+        # 可以 24
+
+
+# 2、使用 DataFrame 存储词语及其出现次数
+def show_txt_words_f2():
+    txt = open("C:\\Users\\Zero\\Desktop\\tools\\Python数据科学手册_机器学习.md", "r", encoding='utf-8').read()
+    words = jieba.lcut(txt)  # 使用精确模式对文本进行分词
     counts = pd.DataFrame(columns=['word', 'count']).set_index('word')
 
     # 对分隔后的文本进行计数
@@ -13,19 +41,16 @@ def show_txt_words():
         if len(word) == 1:  # 单个词语不计算在内
             continue
         else:
-            # counts[word] = counts.get(word, 0) + 1  # 遍历所有词语，每出现一次其对应的值加 1
-            # counts.loc[word] = 1
-            counts.loc[word] = counts.loc[word] + 1  # KeyError
+            try:
+                counts.loc[word] = counts.loc[word] + 1  # 如果该 index 对应的行已存在，则 count 值加一
+            except:
+                new_word = pd.DataFrame(data=[{'word': word, 'count': 1}]).set_index('word')  # data=[{..}]，count 初始化为 1
+                counts = counts.append(new_word, ignore_index=False).sort_values(by='count', ascending=False)  # 不存在，插入
 
-    print(counts)
+    # print(counts)
 
-    # items = list(counts.items())  # 将键值对转换成列表
-    # items.sort(key=lambda x: x[1], reverse=True)  # 根据词语出现的次数进行从大到小排序
-
-    # 输出前 15
-    # for i in range(15):
-    #     word, count = items[i]
-    #     print("{0:<5}{1:>5}".format(word, count))
+    # 输出前 20
+    print(counts.iloc[:20])
 
 
 def show_words():
@@ -37,5 +62,6 @@ def show_words():
 
 if __name__ == '__main__':
     # show_words()
-    show_txt_words()
+    show_txt_words_f1()
+    show_txt_words_f2()
 
