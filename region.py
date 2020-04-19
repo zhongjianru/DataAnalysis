47c6759bddce378559cab0b1439b4db5386d28bf
kinyuchung <459251261@qq.com>
2020-04-19 21:59:31 +0800

    处理地域数据

---
+++ region.py
@@ -0,0 +1,31 @@
+# Time : 2020/4/19 4:58 下午 
+
+# Author : zhongjr
+
+# File : region.py 
+
+# Purpose: region.xls
+
+import pandas as pd
+
+file = open('region.sql').read()
+columns = ['id', 'name', 'pid', 'sname', 'level', 'citycode', 'zipcode', 'mername', 'lng', 'lat', 'phonetic']
+df_region = pd.DataFrame(columns=columns)
+df = pd.DataFrame(columns=columns)
+# df_region = df = pd.DataFrame(columns=columns)
+# 不能这样写，这样会创建一个df_region到df的引用，df的值改变的话，df_region也会跟着改变，会导致后面数据重复
+cnt = 0
+
+for line in file.split('\n'):
+    if line.find('INSERT') == 0:
+        data = line.replace('INSERT INTO `region` VALUES (', '').replace(');', '').replace("'", '').split(', ')
+        df.loc[cnt] = data
+        cnt = cnt + 1
+        if cnt % 100 == 0:
+            df_region = df_region.append(df, ignore_index=True)  # 分批提交
+            df.drop(index=df.index, inplace=True)  # 清空df的行
+            print(cnt)
+
+df_region = df_region.append(df)
+df_region.to_excel('region.xls')
+print('write down:', cnt)
