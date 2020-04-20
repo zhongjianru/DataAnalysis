b25222eaa93681af54e7ac5aa0f09c287b524d6f
zhongjianru <459251261@qq.com>
2020-04-20 22:08:24 +0800

    处理地域数据

--- region.py
+++ region.py
@@ -8,7 +8,6 @@
 
 import pandas as pd
 
-file = open('files/region.sql', mode='r', encoding='utf-8').readlines()  # 不用自己分割换行，直接用readlines()
 columns = ['id', 'name', 'pid', 'sname', 'level', 'citycode', 'zipcode', 'mername', 'lng', 'lat', 'phonetic']
 df_region = pd.DataFrame(columns=columns)
 df = pd.DataFrame(columns=columns)
@@ -16,15 +15,17 @@ df = pd.DataFrame(columns=columns)
 # 不能这样写，这样会创建一个df_region到df的引用，df的值改变的话，df_region也会跟着改变，会导致后面数据重复
 cnt = 0
 
-for line in file:
-    if line.find('INSERT') == 0:
-        data = line.replace('INSERT INTO `region` VALUES (', '').replace(');', '').replace("'", '').split(', ')
-        df.loc[cnt] = data
-        cnt = cnt + 1
-        if cnt % 100 == 0:
-            df_region = df_region.append(df, ignore_index=True)  # 分批提交
-            df.drop(index=df.index, inplace=True)  # 清空df的行
-            print(cnt)
+with open('files/region.sql', mode='r', encoding='utf-8') as f:
+    lines = f.readlines()  # 直接用readlines()分割换行
+    for line in lines:
+        if line.find('INSERT') == 0:
+            data = line.replace('INSERT INTO `region` VALUES (', '').replace(');', '').replace("'", '').split(', ')
+            df.loc[cnt] = data
+            cnt = cnt + 1
+            if cnt % 100 == 0:
+                df_region = df_region.append(df, ignore_index=True)  # 分批提交
+                df.drop(index=df.index, inplace=True)  # 清空df的行
+                print(cnt)
 
 df_region = df_region.append(df)
 df_region.to_excel('files/region.xls')
